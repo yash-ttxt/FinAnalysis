b72c9ab4bd682921ee3f8d3f5986b00262f8b3f5
yash-ttxt <yashshah160498@gmail.com>
2025-01-04 14:12:16 +0100

    renaming

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,14 +1,17 @@
+package main
+
 import org.apache.spark.sql.SparkSession
 import io.github.cdimascio.dotenv.Dotenv
 
 object Main {
   def main(args: Array[String]): Unit = {
+    val dotenv = Dotenv.load()
     val spark = SparkSession.builder
-      .master(Dotenv.load().get("SPARK_MASTER"))
       .appName(Dotenv.load().get("SPARK_APP_NAME"))
+      .master(Dotenv.load().get("SPARK_MASTER"))
       .getOrCreate()
 
-    // Add your code here
+    // Add the ETL code here.
 
     spark.stop()
   }
