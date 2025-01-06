0c58600ec76e2dc5b02a1a919a7088e53ec8f88b
yash-ttxt <yashshah160498@gmail.com>
2025-01-06 14:55:48 +0100

    unnecessary repartitions avoided

--- src/main/scala/com/example/bankanalysis/transformation/Aggregations.scala
+++ src/main/scala/com/example/bankanalysis/transformation/Aggregations.scala
@@ -7,8 +7,7 @@ import org.apache.spark.sql.Row
 object Aggregations {
 
   def top10TotalTransactionAmountByCustomer(df: DataFrame): DataFrame = {
-    val partitionedDf = df.repartition(col("email")) // Replace "customerId" with the actual column name used for grouping
-    partitionedDf.groupBy(col("email"))
+    df.groupBy(col("email"))
       .agg(sum(col("transaction_amount"))
         .as("total_transaction_amount"))
       .select("email", "total_transaction_amount")
@@ -17,8 +16,8 @@ object Aggregations {
   }
 
   def monthlyTransactionVolumeByBranch(df: DataFrame): DataFrame = {
-    val partitionedDf = df.withColumn("month", month(col("transaction_date"))).repartition(col("month"), col("branch_id"))
-    partitionedDf.groupBy(col("month"), col("branch_id"))
+    val newDf = df.withColumn("month", month(col("transaction_date")))
+    newDf.groupBy(col("month"), col("branch_id"))
       .agg(sum(col("transaction_amount"))
         .as("transaction_volume"))
       .select("month", "branch_id", "transaction_volume")
--- src/main/scala/com/example/bankanalysis/transformation/WindowFunctions.scala
+++ src/main/scala/com/example/bankanalysis/transformation/WindowFunctions.scala
@@ -6,9 +6,8 @@ import org.apache.spark.sql.expressions.Window
 
 object WindowFunctions {
   def weeklyAverageTransactionAmountByCustomer(df: DataFrame): DataFrame = {
-    val partitionedDf = df.repartition(col("email"))
     val windowSpec = Window.partitionBy("email").orderBy(col("transaction_date").cast("timestamp").cast("Long")).rangeBetween(-7 * 86400, 0)
-    partitionedDf
+    df
       .withColumn("weekly_avg_transaction_amount", avg(col("transaction_amount")).over(windowSpec))
       .select("email", "transaction_date", "transaction_amount", "weekly_avg_transaction_amount")
   }
